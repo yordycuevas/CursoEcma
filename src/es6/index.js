8b6d8217fa99dbe7662ceaa3306a5c90bad84b28
yordycuevas <yordycuevas21@gmail>
2021-12-11 02:21:20 -0500

    avance taller EcmaScrtipt

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,15 +1,117 @@
 function newFunction(name, age, country) {
-    var name = name || 'oscar';
-    var age = age || 32;
-    var country = country || 'MX';
-    console.log(name, age, country);
-  }
-  
-  // es6
+  var name = name || "oscar";
+  var age = age || 32;
+  var country = country || "MX";
   console.log(name, age, country);
-  function newFunction2(name = 'oscar', age = 32, country = "MX") {
-  };
-  
-  newFunction2();
-  newFunction2('Ricardo', '23', 'CO');
-  
+}
+
+// es6
+function newFunction2(name = "oscar", age = 32, country = "MX") {
+  console.log(name, age, country);
+}
+
+newFunction2();
+newFunction2("Ricardo", "23", "CO");
+
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world;
+console.log(epicPhrase);
+
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
+
+// antes js
+
+let lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing \n" +
+  "Lorem ipsum dolor sit amet, consectetur adipiscing";
+
+// nueva forma em6
+let lorem2 = `Lorem ipsum dolor sit amet
+ahora es otra frase epica
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+  name: "oscar",
+  age: 32,
+  country: "MX",
+};
+
+console.log(person.name, person.age);
+
+let { name, age, country } = person;
+console.log(name, age, country);
+
+let team1 = ["oscar", "Julian", "Ricardo"];
+let team2 = ["Valeria", "Yesica", "Camila"];
+
+let education = ["david", ...team1, ...team2];
+
+console.log(education);
+
+{
+  var globalVar = "Global var";
+}
+
+{
+  let globalLet = "Global let";
+
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+const a = "b";
+
+
+// procesos forma
+
+let person1 = {
+  name: "oscar",
+  age: 32,
+  country: "MX",
+};
+
+obj = {name, age};
+console.log(person1);
+
+// Arrow function
+
+const names = [
+{ name: 'oscar', age:32},
+{ name: 'Yordy', age:30}
+
+];
+
+let listOfNames = names.map(function(item){
+  console.log(item.name);
+});
+
+
+// v2
+
+let listOfNames2 = names.map(item => console.log(item.name));
+
+
+// promesas
+
+const helloPromise = () => {
+return new Promise((resolve, reject) => {
+  if (true){
+    resolve('Hey!');
+  }else{
+    reject('Ups !!');
+
+  }
+});
+
+}
+
+helloPromise()
+.then(response => console.log(response))
+.then(() => console.log('Hola'))
+.catch(error => console.log(error));
---
+++ src/es6/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let person1 = {
+  name: "oscar",
+  age: 32,
+  country: "MX",
+};
+
+obj = {name, age};
+console.log(person1);
